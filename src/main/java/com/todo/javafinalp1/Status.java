cb9f5cbb32acc2bb62db7e64b5fc45ac456c26bf
dalibran <dalibrandi@gmail.com>
2024-11-29 19:06:17 -0800

    Added toString methods in enum, for improved FXML text display
    Example Usage:
    Status status //holds Status enum type
    String statusString status.toString() //converts enum to string

--- src/main/java/com/todo/javafinalp1/Status.java
+++ src/main/java/com/todo/javafinalp1/Status.java
@@ -1,8 +1,29 @@
 package com.todo.javafinalp1;
 
 public enum Status {
-    TODO,
-    IN_PROGRESS,
-    COMPLETE,
-    CLOSED
+    TODO {
+        @Override
+        public String toString() {
+            return "Todo";
+        }
+    },
+    IN_PROGRESS {
+        @Override
+        public String toString() {
+            return "In Progress";
+        }
+    },
+    COMPLETE {
+        @Override
+        public String toString() {
+            return "Complete";
+        }
+    },
+    CLOSED {
+        //status for when a user closes a task without completing it
+        @Override
+        public String toString() {
+            return "Closed";
+        }
+    }
 }
